d23479c80f7fe594a01331cbaa5b8f9408357c01
Charles Andrews <cfandrews@outlook.com>
2023-09-13 21:19:26 -0400

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-rm -rf .mypy_cache .pytest_cache .ruff_cache src/python_template.egg_info venv .coverage pyvenv.cfg
+rm -rf .mypy_cache .pytest_cache .ruff_cache src/python_template.egg-info venv .coverage pyvenv.cfg
 set -e
 python3 -m venv ./venv
 ./venv/bin/pip install -e '.[dev]'
+./venv/bin/pre-commit install
 ./venv/bin/isort .
 ./venv/bin/black src
 ./venv/bin/black tests
