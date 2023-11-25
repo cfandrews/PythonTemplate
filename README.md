02776829cc0ca25c7d2649d307a9d96944761b09
Charles Andrews <cfandrews@outlook.com>
2023-11-25 02:32:03 -0500

    Use python-build reusable workflow in build.yml

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![Checked with mypy](https://www.mypy-lang.org/static/mypy_badge.svg)](https://mypy-lang.org/)
 [![Linting: Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/charliermarsh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
 
-Template repository to be used as a base for Python projects
+Template repository to be used as a base for Python projects.
 
-[Source](https://github.com/cfandrews/PythonTemplate/)\
+[Source](https://github.com/cfandrews/PythonTemplate/)  
 [Documentation](https://cfandrews.github.io/PythonTemplate/pythontemplate.html)
--- src/pythontemplate/__init__.py
+++ src/pythontemplate/__init__.py
@@ -1,5 +1,16 @@
 # Copyright 2023 Charles Andrews
-""".. include:: ../../README.md"""  # noqa: D415
+# fmt: off
+"""[![Build Status](https://github.com/cfandrews/PythonTemplate/actions/workflows/build.yml/badge.svg)](https://github.com/cfandrews/PythonTemplate/actions)
+[![Documentation Status](https://github.com/cfandrews/PythonTemplate/actions/workflows/documentation.yml/badge.svg)](https://github.com/cfandrews/PythonTemplate/actions)
+[![Checked with mypy](https://www.mypy-lang.org/static/mypy_badge.svg)](https://mypy-lang.org/)
+[![Linting: Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/charliermarsh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+
+Template repository to be used as a base for Python projects.
+
+[Source](https://github.com/cfandrews/PythonTemplate/)  
+[Documentation](https://cfandrews.github.io/PythonTemplate/contextuallogging.html)
+"""  # noqa: W291, D205, D415
+# fmt: on
 from __future__ import annotations
 
 from typing import Final
